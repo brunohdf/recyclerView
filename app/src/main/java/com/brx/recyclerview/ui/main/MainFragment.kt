f86abae8adec10890862a902eda42cf7af10b2b5
brunohdf <brunuhuu@gmail.com>
2019-02-13 21:01:15 -0200

    snap center approach

--- app/src/main/java/com/brx/recyclerview/ui/main/MainFragment.kt
+++ app/src/main/java/com/brx/recyclerview/ui/main/MainFragment.kt
@@ -4,10 +4,12 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.LinearSnapHelper
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.brx.recyclerview.R
 import kotlinx.android.synthetic.main.main_fragment.*
 import java.util.*
@@ -31,20 +33,81 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
-        var list = mutableListOf<Date>()
+        val list = mutableListOf<Date>()
         val adapter = ListAdapter(list)
+        val linearLayoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = linearLayoutManager
+
+        val snapHelper = LinearSnapHelper()
+
+        snapHelper.attachToRecyclerView(recyclerView)
 
         viewModel.loadData.observe(this, android.arch.lifecycle.Observer { data ->
             data?.let {
                 list.clear()
                 list.addAll(it)
                 adapter.notifyDataSetChanged()
+
+                val centerPosition = it.size / 2
+                linearLayoutManager.scrollToPosition(centerPosition)
+                snapToCenter(linearLayoutManager, centerPosition, snapHelper)
             }
         })
     }
 
+    private fun snapToCenter(
+        linearLayoutManager: LinearLayoutManager,
+        centerPosition: Int,
+        snapHelper: LinearSnapHelper
+    ): Boolean {
+        return recyclerView.post(Runnable {
+            val view = linearLayoutManager.findViewByPosition(centerPosition)
+            if (view == null) {
+                Log.e("POC", "Cant find target View for final snap")
+                return@Runnable
+            }
 
+            val snapDistance = snapHelper.calculateDistanceToFinalSnap(linearLayoutManager, view)
+            snapDistance?.let {
+                if (it[0] != 0 || it[1] != 0) recyclerView.smoothScrollBy(it[0], it[1])
+            }
+        })
+    }
 
+    private fun customSnap(): LinearSnapHelper {
+        return object : LinearSnapHelper() {
+            override fun findTargetSnapPosition(
+                layoutManager: RecyclerView.LayoutManager?,
+                velocityX: Int,
+                velocityY: Int
+            ): Int {
+                val centerView = findSnapView(layoutManager!!) ?: return RecyclerView.NO_POSITION
+                val position = layoutManager.getPosition(centerView)
+                var targetPosition = -1
+
+                if (layoutManager.canScrollHorizontally()) {
+                    targetPosition = if (velocityX < 0) {
+                        position - 1
+                    } else {
+                        position + 1
+                    }
+                }
+
+                if (layoutManager.canScrollVertically()) {
+                    targetPosition = if (velocityY < 0) {
+                        position - 1
+                    } else {
+                        position + 1
+                    }
+                }
+
+                val firstItem = 0
+                val lastItem = layoutManager.itemCount - 1
+                targetPosition = Math.min(lastItem, Math.max(targetPosition, firstItem))
+
+                return targetPosition
+            }
+        }
+    }
 }
--- app/src/main/java/com/brx/recyclerview/ui/main/MainViewModel.kt
+++ app/src/main/java/com/brx/recyclerview/ui/main/MainViewModel.kt
@@ -4,14 +4,20 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import java.util.*
 
+
 class MainViewModel : ViewModel() {
 
     var loadData: MutableLiveData<List<Date>> = MutableLiveData()
 
     init {
+        val cal = Calendar.getInstance()
+        cal.set(Calendar.YEAR, 2019)
+        cal.set(Calendar.MONTH, Calendar.JANUARY)
+        cal.set(Calendar.DAY_OF_MONTH, 1)
+
         val list = mutableListOf<Date>()
-        for (i in 1..10) {
-            list.add(Date().addDays(i))
+        for (i in 0..10) {
+            list.add(cal.time.addDays(i))
         }
 
         loadData.value = list
